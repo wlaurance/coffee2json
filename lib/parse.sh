ef65a909f7e597cdc9b7b3a5a099164ff0a3878f
wlaurance <w.laurance@gmail.com>
2012-04-09 00:07:49 -0400

    Add the final } to replace parse
    bump v 0.0.3

--- lib/parse.sh
+++ lib/parse.sh
@@ -2,7 +2,7 @@
 replace '(?!")(\w+):(?!")' '"$1":' $1 >> /dev/null
 replace '(")(http)(s*)"' 'http$3' $1 >> /dev/null
 replace '(})(\).call\(this\);)' '' $1 >> /dev/null
-replace '(})(\);)' '' $1  >> /dev/null
+replace '(})(\);)' '}' $1  >> /dev/null
 replace '(\(function\(\) {)' ' ' $1 >> /dev/null
 replace '(\({)' '{' $1 >> /dev/null
 cat $1 > $2
